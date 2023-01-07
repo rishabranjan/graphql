e8dadee885055eb15bcc66a8fb7bcc3709d1b3a4
Rishab Ranjan <therishabranjan@gmail.com>
2023-01-07 12:00:51 +0530

    initial server setup

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
 import { ApolloServer, gql } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+
+const typeDefs = gql`
+  type Query {
+    name: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    name: () => "rishab ranjan",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server running on ${url}!`);
+});
